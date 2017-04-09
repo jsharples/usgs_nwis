b0ee4949663dec785a7f1c9996b5296540f95e1f
unknown <jpsharples@gmail.com>
2017-04-09 22:31:56 +1000

    redo base query class

--- usgs_dv/usgs_dv.py
+++ usgs_dv/usgs_dv.py
@@ -1,10 +1,10 @@
 
-__all__ = ['Daily_values', 'Streamflow_sites_DV']
+__all__ = ['Streamflow_sites', 'BaseQuery']
 
 
-class Daily_values(object):
+class BaseQuery(object):
   
-    def __init__(self, major_filter):
+    def __init__(self, major_filter, service='dv', format = 'json'):
         '''
         Args:
            major_filter: must be a dict with a singe key - value pair
@@ -16,7 +16,7 @@ class Daily_values(object):
                countyCd - for a list of county numbers      
        
         '''
-        self._format = {'format':'json'}
+        self._format = {'format': format}
         self.allowed_filters = ["sites","stateCd","huc","bBox","countyCd"]
         
         if list(major_filter.keys())[0] in self.allowed_filters:        
@@ -24,7 +24,7 @@ class Daily_values(object):
         else:
             raise ValueError("major_filter must be one of: {}".format(', '.join(self.allowed_filters)))
         
-        self.base_url = "https://waterservices.usgs.gov/nwis/dv/?"
+        self.base_url = "https://waterservices.usgs.gov/nwis/{}/?".format(service)
         self.data = None
   
     def _make_request_url(self, **kwargs):
@@ -33,8 +33,13 @@ class Daily_values(object):
         kwargs.update(self._format)
         
         for arg in kwargs.keys():
-            if type(kwargs[arg]) is list:
-                kwargs[arg] = ','.join(map(str,kwargs[arg]))
+            
+            try:
+                assert not isinstance(kwargs[arg], str)
+                kwargs[arg] = ','.join(map(str, kwargs[arg]))
+                
+            except:
+                pass
        
         return self.base_url + parse.urlencode(kwargs, doseq=True)
    
@@ -49,16 +54,34 @@ class Daily_values(object):
         self.raw_data = response.read().decode(response.info().get_content_charset('utf-8'))
         self.data = json.loads(self.raw_data)
        
-        data_cnt = sum([len(x['values'][0]['value']) for x in self.data['value']['timeSeries']])
-       
-        print("{} data points found".format(data_cnt))
+        #data_cnt = sum([len(x['values'][0]['value']) for x in self.data['value']['timeSeries']])
+        #print("{} data points found".format(data_cnt))
        
         return self.data
 
    
-    def Daily_valuesError(Exception):
+    def pyUSGSError(Exception):
         pass
-       
+    
+    def _date_parse(self, str_date):
+        
+        if len(str_date) == 29 and str_date[-3]==':':
+            str_date = str_date[:-3]+str_date[-2:]
+            return datetime.strptime(str_date,'%Y-%m-%dT%H:%M:%S.%f%z')
+        
+        if len(str_date) == 23:
+            return datetime.strptime(str_date,'%Y-%m-%dT%H:%M:%S.%f')
+        
+        if len(str_date) == 19:
+            return datetime.strptime(str_date,'%Y-%m-%dT%H:%M:%S')
+        
+        if len(str_date) == 16:
+            return datetime.strptime(str_date,'%Y-%m-%dT%H:%M')
+        
+        if len(str_date) == 10:
+            return datetime.strptime(str_date,'%Y-%m-%d')
+        
+    
     @staticmethod   
     def show_useful_links():
         '''
@@ -79,21 +102,23 @@ class Daily_values(object):
         print('\nLast updated {}'.format(updated))
    
    
-
-
-
-class Streamflow_sites_DV(Daily_values):
+class Streamflow_sites(BaseQuery):
     
-    def __init__(self, sites, start_date, end_date, kwargs=None):
+    def __init__(self, sites, start_date, end_date, service='dv', parameter="00060", kwargs=None):
+        
+        super().__init__(major_filter = {"sites":sites}, service=service)
         
         if not kwargs:
             kwargs = {}
         
-        super().__init__(major_filter = {"sites":sites})
-              
-        kwargs.update({"startDT":datetime.strftime(start_date,'%Y-%m-%d'), 
-                       "endDT":datetime.strftime(end_date,'%Y-%m-%d'),
-                       "parameterCd":"00060"
+        if service == 'iv':
+            _date_formatter = '%Y-%m-%dT%H:%M'
+        else:
+            _date_formatter = '%Y-%m-%d'
+               
+        kwargs.update({"startDT":datetime.strftime(start_date, _date_formatter), 
+                       "endDT":datetime.strftime(end_date, _date_formatter),
+                       "parameterCd":parameter
                       })
         
         self.data = self.get_data(**kwargs)
@@ -107,10 +132,12 @@ class Streamflow_sites_DV(Daily_values):
             core_data.append(dict(
             location = ts['sourceInfo']['geoLocation']['geogLocation'],
             name = ts['sourceInfo']['siteName'],
+            site = ts['sourceInfo']['siteCode'][0]['value'],
             unit = ts['variable']['unit']['unitCode'],
             description = ts['variable']['variableDescription'],
             qual_codes = {x['qualifierCode']: x['qualifierDescription'] for x in ts['values'][0]['qualifier']},
-            data = ts['values'][0]['value']
+            data = ts['values'][0]['value'],
+            time_zone = ts['sourceInfo']['timeZoneInfo']['defaultTimeZone']['zoneOffset']
             ))
         
         self.core_data = core_data
@@ -121,7 +148,9 @@ class Streamflow_sites_DV(Daily_values):
             self.make_core_data()
         
         for ts in self.core_data:
-                      
-            plt.plot([datetime.strptime(x['dateTime'],'%Y-%m-%dT%H:%M:%S.%f') for x in ts['data']],[x['value'] for x in ts['data']])
+            plt.figure(figsize=(15,7))          
+            plt.plot([self._date_parse(x['dateTime']) for x in ts['data']],[x['value'] for x in ts['data']])
+            plt.title(ts['site']+': '+ts['name'])
             plt.show()
         
+
